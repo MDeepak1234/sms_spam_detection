ef766522a2f15a06643d77d7dbf14dd2095ddddb
MDeepak1234 <maramdeepaksimhareddy@gmail.com>
2026-02-07 07:23:35 +0530

    Fix Streamlit Cloud dependency + nltk crash

--- app.py
+++ app.py
@@ -1,42 +1,61 @@
 import streamlit as st
-import pickle 
+import pickle
 import string
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+import os
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-ps=PorterStemmer()
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', quiet=True)
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords', quiet=True)
+
+ps = PorterStemmer()
+
 def text_transform(text):
-    text=text.lower()
-    text=nltk.word_tokenize(text)
-    s=[]
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    words = []
     for i in text:
         if i.isalnum():
-            s.append(i)
-    text=s[:]
-    s.clear()
-    for i in text:
+            words.append(i)
+
+    filtered = []
+    for i in words:
         if i not in stopwords.words('english') and i not in string.punctuation:
-            s.append(i)
-    text=s[:]
-    s.clear()
-    for i in text:
-        s.append(ps.stem(i))
-    return " ".join(s)
-tf=pickle.load(open('vectorizer.pkl','rb'))
-m=pickle.load(open('model.pkl','rb'))
-st.title("SMS Spam Classifier")
-input_sms=st.text_area("Enter the message")
-if st.button('Predict'):
-    #preprocessing
-    text_sms=text_transform(input_sms)
-    #vectorizer
-    vect_inp=tf.transform([text_sms])
-    #model
-    res=m.predict(vect_inp)[0]
-    #output
-    if res==1:
-        st.header("SPAM")
+            filtered.append(i)
+
+    stemmed = []
+    for i in filtered:
+        stemmed.append(ps.stem(i))
+
+    return " ".join(stemmed)
+
+
+tf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title("📩 SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button("Predict"):
+    if input_sms.strip() == "":
+        st.warning("Please enter a message")
     else:
-        st.header("NOT SPAM")
+        transformed_sms = text_transform(input_sms)
+        vector_input = tf.transform([transformed_sms])
+        result = model.predict(vector_input)[0]
+
+        if result == 1:
+            st.error("🚨 SPAM MESSAGE")
+        else:
+            st.success("✅ NOT SPAM")
