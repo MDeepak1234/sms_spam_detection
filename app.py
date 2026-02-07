cd8392dae84f969a516c1c843be43c10c3f80866
MDeepak1234 <maramdeepaksimhareddy@gmail.com>
2026-02-07 07:19:38 +0530

    Fix Streamlit Cloud dependency issues

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import string
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
-nltk.download('punkt_tab')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 ps=PorterStemmer()
